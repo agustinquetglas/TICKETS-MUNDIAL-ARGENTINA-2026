d6bdea5f525dbc9d4ea2ebe2bd2ca150d2307a62
Bautista Goya <goyabautiqgmail.com>
2026-05-06 10:54:25 -0300

    estructura para el formulario de actualizar contraseña

---
+++ client/src/app/login/actualizar-contraseña/page.tsx
@@ -0,0 +1,20 @@
+export default function UpdatePasswordPage() {
+  return (
+    <main className="login-main">
+      <div className="login-card">
+        
+        {/* dejo la imagen de la copa en la izquiera */}
+        <div className="login-image-container">
+          <img 
+            src="/logo-login-verde.png" 
+            alt="Mundial 2026 Verde" 
+            className="login-image" 
+          />
+        </div>
+
+        {/* formulario para actualizar la contraseña a la derecha */}
+        
+      </div>
+    </main>
+  );
+}
