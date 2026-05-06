060a2e057cdda048a5868d2a7aa49c7bf93b7c56
MatiasCostantini <matiascostantini24@gmail.com>
2026-05-06 14:32:14 -0300

    Se elimina actualizarStock porque el stock ahora esta en sector, no en partido

--- server/src/partidos/partidos.repository.ts
+++ server/src/partidos/partidos.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { SupabaseService } from '../supabase/supabase.service';
 
 @Injectable()
@@ -33,20 +33,5 @@ export class PartidosRepository {
         }
 
         return data;
-    }
-
-    async actualizarStock(id: number, nuevoStock: number) {
-        const supabase = this.supabaseService.getClient();
-        const { error } = await supabase
-            .from('Partidos')
-            .update({ stock_disponible: nuevoStock })
-            .eq('id', id);
-
-        if (error) {
-            console.error('Error al actualizar stock en Supabase:', error);
-            throw new InternalServerErrorException('Error al actualizar el stock del partido');
-        }
-
-        return true;
     }
 }
