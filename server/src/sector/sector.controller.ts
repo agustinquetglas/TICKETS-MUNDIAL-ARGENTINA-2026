5ebcbe4ef6288d639ef0e51d4fe0e6869615f3c9
tomy-sego <juantomasegovia05@gmail.com>
2026-05-10 21:18:34 -0300

    solucion de comprobantes paso 1

--- server/src/sector/sector.controller.ts
+++ server/src/sector/sector.controller.ts
@@ -6,7 +6,13 @@ export class SectorController {
     constructor(private readonly sectorRepo: SectorRepository) {}
 
     @Get(':partidoId')
-    async obtenerPorPartido(@Param('partidoId', ParseIntPipe) partidoId: number) {
+    async obtenerPorPartido(@Param('partidoId') partidoIdStr: string) {
+        console.log('RECEIVED PARTIDO ID:', partidoIdStr, 'TYPE:', typeof partidoIdStr);
+        const partidoId = parseInt(partidoIdStr, 10);
+        if (isNaN(partidoId)) {
+            console.log('IT IS NAN!');
+            throw new Error('Invalid numeric string: ' + partidoIdStr);
+        }
         return await this.sectorRepo.obtenerPorPartido(partidoId);
     }
 }
